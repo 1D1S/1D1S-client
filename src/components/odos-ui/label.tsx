2c7c44cb8e16d6483b6850a9a4426c9831855d2a
Soomin <gil2093ab@gmail.com>
2025-05-04 22:24:56 +0900

    feat: add OdosLabel component with size and weight variants

---
+++ src/components/odos-ui/label.tsx
@@ -0,0 +1,36 @@
+import { cn } from '@/lib/utils';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+const odosLabelVariants = cva('', {
+  variants: {
+    size: {
+      heading1: 'text-3xl',
+      heading2: 'text-2xl',
+      body1: 'text-xl',
+      body2: 'text-lg',
+      caption1: 'text-base',
+      caption2: 'text-sm',
+      caption3: 'text-xs',
+      pageTitle: 'font-suite text-3xl',
+    },
+    weight: {
+      bold: 'font-bold',
+      medium: 'font-medium',
+      regular: 'font-regular',
+      light: 'font-light',
+    },
+  },
+  defaultVariants: {
+    size: 'body2',
+    weight: 'medium',
+  },
+});
+
+export function OdosLabel({
+  className,
+  size,
+  weight,
+  ...props
+}: React.ComponentProps<'p'> & VariantProps<typeof odosLabelVariants>): React.ReactElement {
+  return <p className={cn(odosLabelVariants({ size, weight, className }))} {...props} />;
+}
