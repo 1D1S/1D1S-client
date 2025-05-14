1e211447ff45ececa8c070ececd58d5fa5bcac7e
Soomin <gil2093ab@gmail.com>
2025-05-14 14:33:55 +0900

    fix: update CSP header to allow inline styles and fix UI rendering

--- src/shared/interfaces/middleware/headers.ts
+++ src/shared/interfaces/middleware/headers.ts
@@ -9,8 +9,22 @@ import { NextResponse } from 'next/server';
  * @param res NextResponse
  */
 export function headersMiddleware(res: NextResponse): void {
+  const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
   res.headers.set('X-Frame-Options', 'DENY');
-  res.headers.set('Content-Security-Policy', "default-src 'self'");
+  res.headers.set('x-nonce', nonce);
+  res.headers.set('Content-Security-Policy', cspHeader.replace(/\s{2,}/g, ' ').trim());
   // 이 부분은 1시간이 적합한지 논의 필요
   res.headers.set('Cache-Control', 'public, max-age=3600');
 }
