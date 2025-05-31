09fdf0c89228cdf4f8f94467c9bd97e59955067a
NOGUEN <nhg1113@icloud.com>
2025-05-31 19:39:05 +0900

    feat: image section 구현 #12
    일지 카드의 이미지 부분을 구현

--- src/shared/components/odos-ui/diary-card.tsx
+++ src/shared/components/odos-ui/diary-card.tsx
@@ -1,33 +1,64 @@
 'use client';
 
-import React, { ReactElement } from 'react';
+import React from 'react';
 import Image from 'next/image';
-import { Card } from '@/shared/components/ui/card';
 import { OdosLabel } from '@/shared/components/odos-ui/label';
-import { cn } from '@/shared/lib/utils';
+import { OdosButton } from './button';
+import { CircularProgress } from './circular-progress';
 
-interface LikeButtonProps {
+interface ImageSectionProps {
+  imageUrl?: string;
+  alt: string;
   likes: number;
   onClick?(): void;
 }
 
-export function LikeButton({ likes, onClick }: LikeButtonProps): ReactElement {
+export function ImageSection({
+  imageUrl,
+  alt,
+  likes,
+  onClick,
+}: ImageSectionProps): React.ReactElement {
   return (
-    <button
-      type="button"
-      onClick={onClick}
-      className={cn(
-        'inline-flex items-center',
-        'self-start',
-        'bg-main-900 p-2',
-        'cursor-pointer hover:bg-red-500',
-        'transition-colors duration-150'
-      )}
-    >
-      <OdosLabel size="caption2" weight="medium" className="ml-1 flex-none text-white">
-        {likes}
-      </OdosLabel>
-    </button>
+    <div className="relative mb-4 h-[250px] w-[200px] overflow-hidden rounded-xl">
+      <Image
+        src={imageUrl ?? '/images/default-card.png'}
+        alt={alt}
+        width={200}
+        height={250}
+        className="object-cover"
+        onError={(e) => {
+          e.currentTarget.src = '/images/default-card.png';
+        }}
+      />
+
+      {/* 왼쪽 위에 원형 Progress 표시 */}
+      <div className="absolute top-2 left-2 z-20">
+        <CircularProgress value={60} size="lg" strokeWidthVariant="thick" />
+      </div>
+
+      <div className="absolute top-2 right-2 z-20">
+        <OdosLabel
+          size="caption2"
+          weight="medium"
+          className="rounded-md bg-black/50 px-2 py-1 text-white"
+        >
+          NEW
+        </OdosLabel>
+      </div>
+
+      <div className="absolute bottom-2 left-2 z-20">
+        <OdosButton
+          variant="defaultSmall"
+          onClick={() => console.log('tooltip 클릭됨')}
+          className="pr-2 pl-2"
+        >
+          <OdosLabel weight="light" size="caption3" onClick={onClick}>
+            {likes}
+          </OdosLabel>
+        </OdosButton>
+      </div>
+    </div>
   );
 }
 
@@ -52,25 +83,19 @@ export function DiaryCard({
   challengeLabel,
   challengeUrl,
   date,
-}: DiaryCardProps): ReactElement {
+}: DiaryCardProps): React.ReactElement {
   return (
-    <Card className="w-64 rounded-2xl bg-white p-6">
-      <Image
-        width={200}
-        height={250}
-        src={imageUrl ?? '/images/default-card.png'}
+    <div>
+      <ImageSection
+        imageUrl={imageUrl}
         alt={title}
-        className="mb-4 h-64 w-full rounded-xl object-cover"
-        onError={(image) => {
-          image.currentTarget.src = '/images/default-card.png';
-        }}
+        likes={likes}
+        onClick={() => console.log('좋아요!')}
       />
 
-      <OdosLabel size="body1" weight="bold">
+      <OdosLabel size="body1" weight="bold" className="block">
         {title}
       </OdosLabel>
-
-      <LikeButton likes={likes}></LikeButton>
 
       <div className="mb-1 flex items-center text-sm text-gray-500">
         <span>{user}</span>
@@ -80,6 +105,6 @@ export function DiaryCard({
       </div>
 
       <p className="text-sm text-gray-400">{date}</p>
-    </Card>
+    </div>
   );
 }
