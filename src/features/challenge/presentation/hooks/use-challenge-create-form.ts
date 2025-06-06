5cf5ae2910f2ebcec4b74ed431d176ff6f509baf
Soomin <gil2093ab@gmail.com>
2025-06-06 15:14:28 +0900

    feat: update challenge create form schema for step1 page

--- src/features/challenge/presentation/hooks/use-challenge-create-form.ts
+++ src/features/challenge/presentation/hooks/use-challenge-create-form.ts
@@ -5,6 +5,12 @@ import { useForm } from 'react-hook-form';
 
 export const challengeCreateFormSchema = z.object({
   period: z.enum(['ENDLESS', 'LIMITED']),
+  title: z
+    .string()
+    .min(1, '챌린지 제목을 입력해주세요.')
+    .max(50, '챌린지 제목은 50자 이하로 입력해주세요.'),
+  category: z.enum(['DEV', 'EXERCISE', 'BOOK', 'MUSIC', 'STUDY', 'LEISURE', 'ECONOMY']).optional(),
+  description: z.string().max(500, '챌린지 설명은 500자 이하로 입력해주세요.').optional(),
 });
 
 export type ChallengeCreateFormValues = z.infer<typeof challengeCreateFormSchema>;
@@ -14,6 +20,8 @@ export function useChallengeCreateForm(): ReturnType<typeof useForm<ChallengeCre
     resolver: zodResolver(challengeCreateFormSchema),
     defaultValues: {
       period: 'ENDLESS',
+      title: '',
+      description: '',
     },
   });
 
