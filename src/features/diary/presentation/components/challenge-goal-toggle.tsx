d02742ecab27928839be13dfe043b5e59110b661
NOGUEN <nhg1113@icloud.com>
2025-06-07 01:26:39 +0900

    feat: 챌린지 목표 토글 구현 #12

---
+++ src/features/diary/presentation/components/challenge-goal-toggle.tsx
@@ -0,0 +1,59 @@
+// components/ChallengeGoalToggle.tsx
+
+import { OdosLabel } from '@/shared/components/odos-ui/label';
+import { cn } from '@/shared/lib/utils';
+import * as SwitchPrimitive from '@radix-ui/react-switch';
+import { Check } from 'lucide-react';
+import React from 'react';
+
+// props는 기존과 동일하게 선언
+export interface ChallengeGoalToggleProps
+  extends React.ComponentPropsWithoutRef<typeof SwitchPrimitive.Root> {
+  checked: boolean;
+  onCheckedChange(newChecked: boolean): void;
+  label: string;
+  disabled?: boolean;
+}
+
+export const ChallengeGoalToggle = React.forwardRef(
+  (
+    {
+      checked,
+      onCheckedChange,
+      label,
+      disabled = false,
+      className,
+      ...props
+    }: ChallengeGoalToggleProps,
+    ref
+  ) => (
+    <div className={cn('inline-flex cursor-pointer items-center select-none', className)}>
+      <SwitchPrimitive.Root
+        className={cn(
+          'relative inline-flex h-4 w-4 items-center justify-center',
+          'rounded focus:outline-none',
+          disabled ? 'bg-gray-200' : 'data-[state=checked]:bg-main-900 bg-gray-200'
+        )}
+        checked={checked}
+        disabled={disabled}
+        onCheckedChange={onCheckedChange}
+        ref={ref as React.Ref<React.ElementRef<typeof SwitchPrimitive.Root>>}
+        {...props}
+      >
+        <SwitchPrimitive.Thumb
+          className={
+            'flex h-full w-full items-center justify-center text-white opacity-0 data-[state=checked]:opacity-100'
+          }
+          asChild
+        >
+          <Check className="h-4 w-4" />
+        </SwitchPrimitive.Thumb>
+      </SwitchPrimitive.Root>
+      <OdosLabel size={'body2'} weight={'regular'} className="ml-2">
+        {label}
+      </OdosLabel>
+    </div>
+  )
+);
+
+ChallengeGoalToggle.displayName = 'ChallengeGoalToggle';
