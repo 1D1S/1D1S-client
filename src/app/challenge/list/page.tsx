3dec6bb46f8e2134a715c7ecd86ba7f7c61fe373
NOGUEN <nhg1113@icloud.com>
2025-06-09 22:51:01 +0900

    feat: challenge-list UI 구현 #10

---
+++ src/app/challenge/list/page.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import React from 'react';
+import { OdosPageBackground } from '@/shared/components/odos-ui/page-background';
+import { OdosPageTitle } from '@/shared/components/odos-ui/page-title';
+import { OdosSpacing } from '@/shared/components/odos-ui/spacing';
+import { OdosPageWatermark } from '@/shared/components/odos-ui/page-watermark';
+import { ChallengeListItem } from '@/shared/components/odos-ui/challenge-list-item';
+import { OdosFooter } from '@/shared/components/odos-ui/footer';
+
+export default function ChallengeList(): React.ReactElement {
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-center">
+        <OdosPageBackground className="min-h-screen min-w-250 px-5">
+          <OdosSpacing className="h-20" />
+          <OdosPageTitle title="일지 목록" />
+          <OdosSpacing className="h-20" />
+
+          <div className="w-full">
+            {Array.from({ length: 10 }).map((_, idx) => (
+              <ChallengeListItem
+                key={idx}
+                className="mt-6"
+                challengeName={`고라니 밥주기 챌린지 ${idx + 1}`}
+                startDate={'2025.04.05'}
+                endDate={'2025.04.10'}
+                maxParticipants={20}
+                currentParticipants={idx + 1}
+              />
+            ))}
+          </div>
+
+          <OdosSpacing className="h-20" />
+          <OdosPageWatermark />
+          <OdosSpacing className="h-20" />
+        </OdosPageBackground>
+      </div>
+      <OdosFooter />
+    </div>
+  );
+}
