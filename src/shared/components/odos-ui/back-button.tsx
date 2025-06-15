68253e72984fd53a5ab62bc381b95f8219e79df3
NOGUEN <nhg1113@icloud.com>
2025-06-15 13:26:47 +0900

    feat: 뒤로가기 버튼 구현 #22

---
+++ src/shared/components/odos-ui/back-button.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+export function BackButton(): React.ReactElement {
+  const router = useRouter();
+
+  return (
+    <button
+      onClick={() => router.back()}
+      className="rounded-odos-2 shadow-odos-default flex h-15 w-15 cursor-pointer items-center justify-center bg-white px-4 py-4 text-sm transition-colors duration-200 hover:bg-gray-300"
+    >
+      <Image src={'/images/left-chevron'} width={24} height={24} alt="back-icon"></Image>
+    </button>
+  );
+}
