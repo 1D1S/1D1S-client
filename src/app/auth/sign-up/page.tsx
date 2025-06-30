de56e823ecb270e7246d279f3120320f2ae075bb
Soomin <gil2093ab@gmail.com>
2025-06-30 14:10:35 +0900

    fix: remove menu text and div for sign-up page

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -6,11 +6,9 @@ export default function SignUpPage(): React.ReactElement {
   return (
     <div className="flex flex-col">
       <div className="flex justify-center">
-        <div className="w-full">menu</div>
         <OdosPageBackground className="min-h-screen min-w-150 px-5">
           <SignUpForm />
         </OdosPageBackground>
-        <div className="w-full" />
       </div>
     </div>
   );
