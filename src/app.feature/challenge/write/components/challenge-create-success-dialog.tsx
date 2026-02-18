4feb53efad3692191b54b6f11ab4f264c045622e
Hyeok Lee <c235335@g.hongik.ac.kr>
2026-02-18 20:00:41 +0900

    fix: challenge/create success-dialog 간격 조정

--- src/app.feature/challenge/write/components/challenge-create-success-dialog.tsx
+++ src/app.feature/challenge/write/components/challenge-create-success-dialog.tsx
@@ -20,11 +20,11 @@ export function ChallengeCreateSuccessDialog({
 }: React.ComponentProps<typeof Dialog>): React.ReactElement {
   return (
     <Dialog {...props}>
-      <DialogContent className="min-w-150 items-center gap-25 px-8 py-12">
-        <DialogHeader className="items-center">
+      <DialogContent className="flex min-w-120 flex-col items-center gap-16">
+        <DialogHeader className="items-center mt-8">
           <DialogTitle>
             <DialogDescription>
-              <Text size="display1" weight="bold" className="text-black">
+              <Text size="display2" weight="bold" className="text-black">
                 챌린지 생성이 완료되었습니다!
               </Text>
             </DialogDescription>
@@ -35,7 +35,7 @@ export function ChallengeCreateSuccessDialog({
             <Image src="/images/check.png" alt="success" width="75" height="75" />
           </div>
         </div>
-        <DialogFooter className="justify-center sm:justify-center">
+        <DialogFooter className="justify-center mb-8">
           <DialogClose asChild>
             <Button variant="default" type="button" className="w-37.5">
               확인
