dee72347674e79e45b7fd129d427ee4496d7395b
NOGUEN <nhg1113@icloud.com>
2026-03-24 21:29:36 +0900

    feat: 다른 사람 프로필 기능 추가

--- src/app.feature/member/consts/query-keys.ts
+++ src/app.feature/member/consts/query-keys.ts
@@ -1,4 +1,5 @@
 export const MEMBER_QUERY_KEYS = {
   sidebar: () => ['member', 'sidebar'] as const,
   myPage: () => ['member', 'myPage'] as const,
+  profile: (memberId: number) => ['member', 'profile', memberId] as const,
 };
